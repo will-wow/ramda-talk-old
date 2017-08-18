5f2d50684f360da0190616b6c5358670854abd68
Will Ockelmann-Wagner <william.lee.wagner@gmail.com>
2017-08-18 02:54:25 -0700

    yeoman scaffold + deps

---
+++ src/ramda.ts
@@ -0,0 +1,6 @@
+// We have a bag of marbles of different colors, and we want to do sort them in different ways.
+
+interface Marble {
+  size: 'small' | 'medium' | 'large';
+  color: 'red' | 'blue' | 'green';
+}
